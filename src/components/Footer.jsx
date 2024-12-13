5ec33137e9c541e0faed47805a9fb97739de2bad
alessiotinaglia <alessio.tina1998@gmail.com>
2024-12-13 15:59:21 +0100

    sistemazione card

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function footer() {
     return(
         <footer>
-            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed est doloremque placeat blanditiis quisquam maxime nemo ipsam quasi, quae veniam tempora dolore quos delectus, eius ut aliquam cum consequatur dicta voluptatem modi fugiat iste? Dolore officiis voluptatibus iste quidem consequuntur possimus culpa laborum maxime, assumenda similique corporis exercitationem et! Libero.</p>
+            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed est doloremque placeat blanditiis quisquam maxime nemo ipsam quasi, quae veniam tempora dolore quos delectus, eius ut aliquam cum consequatur dicta </p>
         </footer>
     )
 }
